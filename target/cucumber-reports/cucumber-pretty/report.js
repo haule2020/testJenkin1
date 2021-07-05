109292a3b95a4961474af97494f1d51b286cbf61
haule2020 <huyhau2015@gmail.com>
2021-07-05 22:51:35 +0700

    d

--- target/cucumber-reports/cucumber-pretty/report.js
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -103,7 +103,7 @@ formatter.match({
   "location": "NegativeSearch.iAlreadyAuthorozied()"
 });
 formatter.result({
-  "duration": 4853273000,
+  "duration": 3949221600,
   "status": "passed"
 });
 formatter.match({
@@ -116,7 +116,7 @@ formatter.match({
   "location": "NegativeSearch.iCallAPIToSearchByInputingInvalidPolicyNumber(String)"
 });
 formatter.result({
-  "duration": 1752683500,
+  "duration": 2084050300,
   "status": "passed"
 });
 formatter.match({
@@ -129,7 +129,7 @@ formatter.match({
   "location": "NegativeSearch.systemResponseNoContentWithStatusCode(String)"
 });
 formatter.result({
-  "duration": 3114800,
+  "duration": 1884200,
   "error_message": "java.lang.AssertionError: expected:\u003c204\u003e but was:\u003c404\u003e\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.failNotEquals(Assert.java:834)\r\n\tat org.junit.Assert.assertEquals(Assert.java:645)\r\n\tat org.junit.Assert.assertEquals(Assert.java:631)\r\n\tat stepDefinitions.NegativeSearch.systemResponseNoContentWithStatusCode(NegativeSearch.java:39)\r\n\tat ✽.Then system response no content with status code \"204\"(resources/Feature/policyEnquery.feature:9)\r\n",
   "status": "failed"
 });
@@ -164,262 +164,4 @@ formatter.step({
 });
 formatter.match({
   "location": "NegativeSearch.iAlreadyAuthorozied()"
-});
-formatter.result({
-  "duration": 302265700,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "%2816548240%",
-      "offset": 56
-    }
-  ],
-  "location": "NegativeSearch.iCallAPIToSearchByInputingInvalidPolicyNumber(String)"
-});
-formatter.result({
-  "duration": 865495700,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "204",
-      "offset": 45
-    }
-  ],
-  "location": "NegativeSearch.systemResponseNoContentWithStatusCode(String)"
-});
-formatter.result({
-  "duration": 623800,
-  "error_message": "java.lang.AssertionError: expected:\u003c204\u003e but was:\u003c404\u003e\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.failNotEquals(Assert.java:834)\r\n\tat org.junit.Assert.assertEquals(Assert.java:645)\r\n\tat org.junit.Assert.assertEquals(Assert.java:631)\r\n\tat stepDefinitions.NegativeSearch.systemResponseNoContentWithStatusCode(NegativeSearch.java:39)\r\n\tat ✽.Then system response no content with status code \"204\"(resources/Feature/policyEnquery.feature:9)\r\n",
-  "status": "failed"
-});
-formatter.scenario({
-  "line": 15,
-  "name": "Search policy infomation by invalid policy number",
-  "description": "",
-  "id": "search-policy-infomation;search-policy-infomation-by-invalid-policy-number;;4",
-  "type": "scenario",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 7,
-  "name": "I already authorozied",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 8,
-  "name": "I call API to search by inputing invalid policy number \"\"",
-  "matchedColumns": [
-    0
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 9,
-  "name": "system response no content with status code \"204\"",
-  "matchedColumns": [
-    1
-  ],
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "NegativeSearch.iAlreadyAuthorozied()"
-});
-formatter.result({
-  "duration": 672668600,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "",
-      "offset": 56
-    }
-  ],
-  "location": "NegativeSearch.iCallAPIToSearchByInputingInvalidPolicyNumber(String)"
-});
-formatter.result({
-  "duration": 579774400,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "204",
-      "offset": 45
-    }
-  ],
-  "location": "NegativeSearch.systemResponseNoContentWithStatusCode(String)"
-});
-formatter.result({
-  "duration": 199900,
-  "error_message": "java.lang.AssertionError: expected:\u003c204\u003e but was:\u003c404\u003e\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.failNotEquals(Assert.java:834)\r\n\tat org.junit.Assert.assertEquals(Assert.java:645)\r\n\tat org.junit.Assert.assertEquals(Assert.java:631)\r\n\tat stepDefinitions.NegativeSearch.systemResponseNoContentWithStatusCode(NegativeSearch.java:39)\r\n\tat ✽.Then system response no content with status code \"204\"(resources/Feature/policyEnquery.feature:9)\r\n",
-  "status": "failed"
-});
-formatter.scenarioOutline({
-  "line": 17,
-  "name": "Search policy infomation by valid policy number",
-  "description": "",
-  "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number",
-  "type": "scenario_outline",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 18,
-  "name": "I already get authorization",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 19,
-  "name": "I call API to search by inputing valid policy number \"\u003cpolicyNumber\u003e\"",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 20,
-  "name": "system response with status code \"\u003cstatusCode\u003e\"",
-  "keyword": "Then "
-});
-formatter.examples({
-  "line": 22,
-  "name": "",
-  "description": "",
-  "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;",
-  "rows": [
-    {
-      "cells": [
-        "policyNumber",
-        "statusCode"
-      ],
-      "line": 23,
-      "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;;1"
-    },
-    {
-      "cells": [
-        "2816548240",
-        "200"
-      ],
-      "line": 24,
-      "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;;2"
-    },
-    {
-      "cells": [
-        "2871284582",
-        "200"
-      ],
-      "line": 25,
-      "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;;3"
-    }
-  ],
-  "keyword": "Examples"
-});
-formatter.scenario({
-  "line": 24,
-  "name": "Search policy infomation by valid policy number",
-  "description": "",
-  "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;;2",
-  "type": "scenario",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 18,
-  "name": "I already get authorization",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 19,
-  "name": "I call API to search by inputing valid policy number \"2816548240\"",
-  "matchedColumns": [
-    0
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 20,
-  "name": "system response with status code \"200\"",
-  "matchedColumns": [
-    1
-  ],
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "SearchByValidParameters.iAlreadyAuthorozied()"
-});
-formatter.result({
-  "duration": 326273600,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2816548240",
-      "offset": 54
-    }
-  ],
-  "location": "SearchByValidParameters.iCallAPIToSearchByInputingValidPolicyNumber(String)"
-});
-formatter.result({
-  "duration": 607774100,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "200",
-      "offset": 34
-    }
-  ],
-  "location": "SearchByValidParameters.systemResponseWithStatusCode(String)"
-});
-formatter.result({
-  "duration": 887700,
-  "error_message": "java.lang.AssertionError: expected:\u003c200\u003e but was:\u003c404\u003e\r\n\tat org.junit.Assert.fail(Assert.java:88)\r\n\tat org.junit.Assert.failNotEquals(Assert.java:834)\r\n\tat org.junit.Assert.assertEquals(Assert.java:645)\r\n\tat org.junit.Assert.assertEquals(Assert.java:631)\r\n\tat stepDefinitions.SearchByValidParameters.systemResponseWithStatusCode(SearchByValidParameters.java:34)\r\n\tat ✽.Then system response with status code \"200\"(resources/Feature/policyEnquery.feature:20)\r\n",
-  "status": "failed"
-});
-formatter.scenario({
-  "line": 25,
-  "name": "Search policy infomation by valid policy number",
-  "description": "",
-  "id": "search-policy-infomation;search-policy-infomation-by-valid-policy-number;;3",
-  "type": "scenario",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 18,
-  "name": "I already get authorization",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 19,
-  "name": "I call API to search by inputing valid policy number \"2871284582\"",
-  "matchedColumns": [
-    0
-  ],
-  "keyword": "When "
-});
-formatter.step({
-  "line": 20,
-  "name": "system response with status code \"200\"",
-  "matchedColumns": [
-    1
-  ],
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "SearchByValidParameters.iAlreadyAuthorozied()"
-});
-formatter.result({
-  "duration": 259105200,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "2871284582",
-      "offset": 54
-    }
-  ],
-  "location": "SearchByValidParameters.iCallAPIToSearchByInputingValidPolicyNumber(String)"
 });
